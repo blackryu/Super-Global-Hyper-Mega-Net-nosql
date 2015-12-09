362c756e29e9927abce900727aa8e2cfb9f33b40
Juan Manuel <junoyer4@gmail.com>
2015-12-09 17:01:31 +0100

    minor fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/list' listRoutes);
+app.use('/list', listRoutes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
