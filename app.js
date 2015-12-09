cd3cfa61848815bb24f3e8015ce3a62168b3eb72
Juan Manuel <junoyer4@gmail.com>
2015-12-09 21:14:07 +0100

    added secret settings and test mongooge connection

--- app.js
+++ app.js
@@ -4,22 +4,39 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//Routes
+// Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var listRoutes = require('./routes/list');
 var todosRoutes = require('./routes/todo');
+var mongoose = require('mongoose');
 var app = express();
 
+// Sectret Config
+var config = require(. / secret.js);
+
+// DB
+var mongooseConnection = 'mongodb://' + config.dbUser + ':' + config.dbPass + '@' + config.dbUrl + '/' + config.dbName;
+
+var db = mongoose.connection;
+
+db.once('open', function(callback){
+    
+    console.log('yay we conected :) ');
+    callback();
+    });
+
+
+mongoose.connect(mongooseConnection);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended : false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,34 +46,27 @@ app.use('/list', listRoutes);
 app.use('/todos', todosRoutes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+if(app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', { message : err.message, error : err });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', { message : err.message, error : {} });
 });
-
 
 module.exports = app;
