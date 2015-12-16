ca1fcb9d8b8cc36337fe891ce2257197bf412f05
chappy2 <Sabrina-Sachs87@web.de>
2015-12-16 20:22:39 +0100

    Fixed the 'Uncaught ReferenceError: $ is not defined(anonymous function) @ main.js:1 $' bug. The main.js is loaded before query is loaded. So main.js is now loaded as the last script import.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,14 @@
-$('#myModal').on('shown.bs.modal', function () {
-  //$('#myInput').focus()
-    var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever')
+
+    $('#myModal').on('shown.bs.modal', function () {
+    //$('#myInput').focus()
+        /*
+        TODO: for a new todo get the list flag and show it
+        in modal as information. So you see for what list you are creating an modal.
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever')
   
-    var modal = $(this)
-  modal.find('.modal-title').text('New Todo for List ' + recipient)
-  modal.find('.modal-body input').val(recipient)
-})
+        var modal = $(this)
+        modal.find('.modal-title').text('New Todo for List ' + recipient)
+        modal.find('.modal-body input').val(recipient)*/
+    });
+
