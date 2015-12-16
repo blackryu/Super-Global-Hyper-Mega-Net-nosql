d84df79b06e02160f656b07e83f377cdb4a6ecc4
chappy2 <Sabrina-Sachs87@web.de>
2015-12-16 19:41:37 +0100

    ng-templates in body.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 $('#myModal').on('shown.bs.modal', function () {
   //$('#myInput').focus()
+    var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever')
+  
+    var modal = $(this)
+  modal.find('.modal-title').text('New Todo for List ' + recipient)
+  modal.find('.modal-body input').val(recipient)
 })
