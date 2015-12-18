8546a9037e62133ebde688e5beb5a4d30c3870de
Juan Manuel <junoyer4@gmail.com>
2015-12-18 13:56:33 +0100

    logic for list collections

--- dbModels/lists.js
+++ dbModels/lists.js
@@ -1,16 +1,17 @@
 var mongoose = require('mongoose');
 var listSchema = mongoose.Schema({
     
-    _name: {type: String, unique: true, index: true, required: true},
-    owners: {type: Array, required: true},
-    visibility: {type: Boolean, required: true},
+    _name: {type: String, unique: true, required: true, index: true},
+    owner: {type: String, required: true, index: true},
+    visibility: {type: String, required: true, default: 'private'},
     color: {type: String, default: '#e95e01'},
     activ: {type: Boolean, default: true},
     todos: []
     
     }, {_id: false});
-    
-    
+        
+    listSchema.index({_name: 1, owner: 1});
+    listSchema.index({owner: 1, visibility: 1});
 var ListModel = mongoose.model('Lists', listSchema);
 
 module.exports = ListModel;
