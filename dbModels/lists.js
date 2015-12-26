9e589c594ebc5809c397d82fb571f4950c6515fc
Juan Manuel <junoyer4@gmail.com>
2015-12-26 21:18:14 +0000

    clean up linst indexes

--- dbModels/lists.js
+++ dbModels/lists.js
@@ -3,13 +3,13 @@ var schema = mongoose.Schema;
 var listSchema = new schema({
     
     _name: {type: String, required: true},
-    owner: {type: String, required: true, unique: false, index: true},
+    owner: {type: String, required: true, index: true},
     visibility: {type: String, default: 'private'},
     color: {type: String, default: '#e95e01'},
     activ: {type: Boolean, default: true},
     todos: []
     });        
-    listSchema.index({_name: 1, owner: 1});
+    listSchema.index({_name: 1, owner: 1}, {unique: true});
     listSchema.index({owner: 1, visibility: 1});
 var ListModel = mongoose.model('Lists', listSchema);
 
