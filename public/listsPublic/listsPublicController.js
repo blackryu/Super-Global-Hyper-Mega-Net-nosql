d5f3aa2f06ff947d705b57d5222babcef0bf1fb6
chappy2 <chappy2@gmx.de>
2015-12-28 14:26:31 +0000

    share public lists done.

--- public/listsPublic/listsPublicController.js
+++ public/listsPublic/listsPublicController.js
@@ -1,8 +1,19 @@
- angular.module('app.listsPublic', ['ngRoute', 'ngResource','app.listsPublicService'])
- .controller('ListControllerPublic',['$scope','ListsPublicAll',function($scope,ListsPublicAll){
+ angular.module('app.listsPublic', ['ngRoute', 'ngResource','app.listsPublicService','app.listsService'])
+ .controller('ListControllerPublic',['$scope','ListsPublicAll','Lists',function($scope,ListsPublicAll,Lists,index){
     
 $scope.lists = ListsPublicAll.query();
      
+     $scope.share=function(index){
+         alert(index);
+         var newList=new Lists({
+             _name:$scope.lists[index]._name,
+             color:$scope.lists[index].color,
+             visibility:$scope.lists[index].visibility,
+             todos:$scope.lists[index].todos
+         });
+         
+         newList.$update();
+     }
 
     }])
      .config(['$routeProvider', function ($routeProvider) {
